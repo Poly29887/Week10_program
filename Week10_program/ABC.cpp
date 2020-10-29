682067df447bc2a5d534f61ae84076515197f54c
TANAPORN SRIBOONPHONG <tanapon1325@gmail.com>
2020-10-29 22:50:26 +0700

    Add project files.

---
+++ Week10_program/ABC.cpp
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int num[3];
+
+void arrange();
+int main()
+{
+    int i;
+    char order[4],*p;
+    printf("Input 3 numbers : ");
+    for (i = 0; i < 3; i++)
+    {
+        scanf("%d", &num[i]);
+    }
+    p = order;
+    printf("Input ABC : ");
+    scanf("%s",order);
+    while (*p != '\0')
+    {
+        if ((*p != 'A')&&(*p != 'B')&&(*p != 'C'))
+        {
+            printf("Input ABC : ");
+            scanf("%s", order);
+            p = order;
+            continue;
+        }
+        p++;
+    }
+    arrange();
+    printf("Output : ");
+    for (i = 0; i < 3; i++)
+    {
+        if (order[i] == 'A')
+        {
+            printf("%d ", num[0]);
+        }
+        else if (order[i] == 'B')
+        {
+            printf("%d ", num[1]);
+        }
+        else if (order[i] == 'C')
+        {
+            printf("%d ", num[2]);
+        }
+    }
+}
+
+void arrange()
+{
+    int j,temp;
+    for (j = 0; j < 2; j++)
+    {     
+        if (num[j] > num[j + 1])
+        {
+            temp = num[j];
+            num[j] = num[j + 1];
+            num[j + 1] = temp;
+        }
+    }
+}
